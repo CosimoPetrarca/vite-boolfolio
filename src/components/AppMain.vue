9a9e9735452857a030a179cab9e9ec21f7379c54
P4anix <pancok_1@yahoo.it>
2023-05-26 07:36:45 +0200

    img non visualizzata

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,27 +1,19 @@
 <template>
     <main>
-        <div class="container">
-
-
+        <div class="container text-center">
             <div class="row my-4 gy-4">
+                <h1>My Projects</h1>
                 <div class="col col-md-4" v-for="project in data">
                     <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
+                        <img v-if="project.image" :src="project.image" class="card-img-top">
                         <div class="card-body">
                             <h5 class="card-title">{{ project.title }}</h5>
                             <p class="card-text">{{ project.description }}</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <a href="#" class="btn btn-primary">Dettaglio</a>
                         </div>
                     </div>
-
                 </div>
             </div>
-
-
-
-
-
-
         </div>
     </main>
 </template>
