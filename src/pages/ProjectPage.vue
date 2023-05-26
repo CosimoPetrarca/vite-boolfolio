7ad3e78cc2fd204560309a72496f88c97b13694a
P4anix <pancok_1@yahoo.it>
2023-05-26 19:01:14 +0200

    visualizzazione show

--- src/pages/ProjectPage.vue
+++ src/pages/ProjectPage.vue
@@ -1,17 +1,53 @@
 <script>
+import axios from 'axios';
 export default {
-    name: "ProjectPage"
+    name: "ProjectPage",
+    data() {
+        return {
+            apiBaseUrl: 'http://127.0.0.1:8000/api',
+            apiUrls: {
+                projects: '/projects'
+            },
+            project: null,
+            isError: false,
+            errorMessage: null
+        }
+    },
+    methods: {
+        getProject() {
+            axios.get(this.apiBaseUrl + this.apiUrls.projects + "/" + this.$route.params.slug)
+                .then((response) => {
+                    console.log(response);
+                    this.project = response.data.result;
+                })
+                .catch((error) => {
+                    console.log(error);
+                    this.isError = true;
+                    this.errorMessage = error.message;
+                })
+        }
+    },
+    created() {
+        this.getProject();
+    }
 }
 </script>
 
 
 <template>
-    <section>
-      <h1 class="mt-5 mb-3">Pagina Progetto</h1>
+    <section v-if="project">
+        <div class="container">
+            <h1 class="mt-5 mb-3">{{ project.title }}</h1>
+            <img v-if="project.image" :src="project.image" class="card-img-top">
+            <p>{{ project.description }}</p>
+        </div>
+    </section>
+    <section v-if="isError">
+        <div class="container">
+            <p>{{ errorMessage }}</p>
+        </div>
     </section>
 </template>
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
